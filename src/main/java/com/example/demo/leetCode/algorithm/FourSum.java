76073cf801f09467397475dcd8f37c56a485cda4
摆渡人 <chenxiaoyang@yuantu.onaliyun.com>
2020-07-08 14:50:26 +0800

    '添加master'

--- src/main/java/com/example/demo/leetCode/algorithm/FourSum.java
+++ src/main/java/com/example/demo/leetCode/algorithm/FourSum.java
@@ -24,6 +24,7 @@ import java.util.List;
  */
 public class FourSum {
     public static List<List<Integer>> fourSum(Integer[] nums, int target) {
+        System.out.println("master");
         List<List<Integer>> all = new ArrayList<>(10);
         List<Integer> list = new ArrayList<>();
         list.addAll(Arrays.asList(nums));
